73e8f7562b64759c37f4b037c1827cd4e0d32575
TW Yang <twy@30abysses.com>
2021-08-10 22:13:37 -0400

    2021-Aug-10 updates
    * Correct Template ReadMe script.
    * Remove HTML `Version`.

--- Template/Code/ClassLibrary/HtmlPage.cs
+++ Template/Code/ClassLibrary/HtmlPage.cs
@@ -5,42 +5,34 @@ namespace ClassLibrary
 {
     public record HtmlPage : Page
     {
-        static readonly SegmentMarker Version = new(
-            Start:
-                "<!DOCTYPE html>" + Newline +
-                "<!-- Version QARATi`HL$m<xWb&6Bykj0Ra$El$%nov7BYjO*u>I -->" + Newline +
-                "<!-- Version: ",
-            End:
-                " -->" + Newline +
-                "<!-- Version Ddv9CJi*WdVFd/5hW2o`hdrt;D7OhiD1;8dAuaJ9r -->"
-        );
-
         static readonly SegmentMarker HtmlLang = new(
             Start:
+                Newline +
                 "<!-- HtmlLang CcBpp/7GB\"XMD#c:5eBV,\\pX~Ha2W6P7_f,=m}+56 -->" + Newline +
                 "<html lang=",
             End:
                 ">" + Newline +
-                "<!-- HtmlLang GZuB|*3;H=xg:B'Az2559~A<QK1EQ=;n<=wg,3pB\" -->");
+                "<!-- HtmlLang GZuB|*3;H=xg:B'Az2559~A<QK1EQ=;n<=wg,3pB\" -->" + Newline);
 
         public static readonly SegmentMarker Title = new(
             Start:
-                "<!-- Title &*Id@2O~~c@j^]<YCh{8/t(DJ1\"+ilLm-1q-uvHrc -->" + Newline +
+                Newline +
+                "    <!-- Title &*Id@2O~~c@j^]<YCh{8/t(DJ1\"+ilLm-1q-uvHrc -->" + Newline +
                 "    <title>",
             End:
                 "</title>" + Newline +
-                "    <!-- Title mKDJo*fWc\"c1Iprcy%~UJdWAip)(.h/I%^\\Z>zbqm -->");
+                "    <!-- Title mKDJo*fWc\"c1Iprcy%~UJdWAip)(.h/I%^\\Z>zbqm -->" + Newline);
 
         public static readonly SegmentMarker MarkdownInput = new(
             Start:
-                "<!-- MarkdownInput XUSmR^t4:hNGdI``s#T)Yt^'tt=)~TS#<JNY3#+v) -->" + Newline +
+                Newline +
+                "    <!-- MarkdownInput XUSmR^t4:hNGdI``s#T)Yt^'tt=)~TS#<JNY3#+v) -->" + Newline +
                 "    <textarea id=\"Markdown-input\" cols=\"80\" rows=\"24\">" + Newline,
             End:
                 "    </textarea>" + Newline +
-                "    <!-- MarkdownInput tbDQ17gb%Fv[<K9b7j9Iikei1qIWJ@H32PkobmOby -->");
+                "    <!-- MarkdownInput tbDQ17gb%Fv[<K9b7j9Iikei1qIWJ@H32PkobmOby -->" + Newline);
 
         public static readonly ReadOnlyCollection<SegmentMarker> Markers = new(new[] {
-            Version,
             HtmlLang,
             Title,
             MarkdownInput });
--- Template/ReadMe.md
+++ Template/ReadMe.md
@@ -20,14 +20,14 @@ See [`refresh-pages.bash`](refresh-pages.bash).
 myWorkspacePath=$(git rev-parse --show-toplevel)
 pushd "${myWorkspacePath}"
 
-pushd Template/Theme/
+pushd Template/Theme/.lib/
 # Bootstrap: https://github.com/twbs/bootstrap/tags
 curl  --remote-name --url https://raw.githubusercontent.com/twbs/bootstrap/v5.0.1/dist/js/bootstrap.bundle.min.js
 # Bootswatch: https://github.com/thomaspark/bootswatch/tags
 curl  --remote-name --url https://raw.githubusercontent.com/thomaspark/bootswatch/v5.0.1/dist/cyborg/bootstrap.min.css
 popd
 
-pushd Template/Markdown-to-HTML/
+pushd Template/Markdown-to-HTML/.lib/
 # CommonMark: https://github.com/commonmark/commonmark.js/tags
 curl  --remote-name --url https://raw.githubusercontent.com/commonmark/commonmark.js/0.29.3/dist/commonmark.min.js
 popd
@@ -37,7 +37,7 @@ popd
 
 ## [Highlight.js](https://github.com/highlightjs/highlight.js/tags)
 
-[`/Template/Syntax-Highlighting/highlight.min.js`](Syntax-Highlighting/highlight.min.js)
+[`/Template/Syntax-Highlighting/.lib/highlight.min.js`](Syntax-Highlighting/.lib/highlight.min.js)
 is a custom-built, slim version of Highlight.js.  See
 [How to Build Highlight.js](https://www.twy30.com/Pages/Highlight.js-build.html)
 for more details.
